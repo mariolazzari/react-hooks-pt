fa5f64babe4641de3350372fdad9d4a2de9aa7b2
Mario Lazzari <mario.lazzari@gmail.com>
2021-10-09 21:52:04 +0200

    Added useEffect

--- src/UseEffect/UseEffect.js
+++ src/UseEffect/UseEffect.js
@@ -1,7 +1,32 @@
+import { useEffect, useState } from "react";
+import "./UseEffect.css";
+import axios from "axios";
+
 const UseEffect = () => {
+  const [loading, setLoading] = useState(true);
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("https://jsonplaceholder.typicode.com/comments")
+      .then(res => {
+        setData(res.data);
+        setLoading(false);
+      })
+      .catch(err => console.log(err));
+  }, []);
+
   return (
-    <div>
-      <h2>use effect</h2>
+    <div className="emails">
+      {loading ? (
+        <p>loading...</p>
+      ) : (
+        <ul>
+          {data.map(item => (
+            <li key={item.id}>{item.email}</li>
+          ))}
+        </ul>
+      )}
     </div>
   );
 };
