a3cfae92c3c13860f70c3b233d1bf9f9333ad578
Mario Lazzari <mario.lazzari@gmail.com>
2021-10-09 21:07:13 +0200

    Added useReducer

--- src/App.js
+++ src/App.js
@@ -1,20 +1,21 @@
 import { useState } from "react";
 import "./App.css";
 import UseState from "./UseState/UseState";
+import UseReducer from "./UseReducer/UseReducer";
 import UseEffect from "./UseEffect/UseEffect";
 
-const hooks = [
-  { id: 1, label: "useState" },
-  { id: 2, label: "useEffect" },
-];
+const hooks = ["useState", "useReducer", "useEffect"];
 
 function App() {
   const [selected, setSelected] = useState(1);
 
   const renderHook = () => {
     switch (selected) {
-      case 1:
+      case 0:
         return <UseState />;
+
+      case 1:
+        return <UseReducer />;
 
       case 2:
         return <UseEffect />;
@@ -29,13 +30,13 @@ function App() {
       <div className="list">
         <h2>React hooks</h2>
         <ul>
-          {hooks.map(hook => (
+          {hooks.map((hook, i) => (
             <li
-              key={hook.id}
-              className={hook.id === selected ? "selected" : null}
-              onClick={() => setSelected(hook.id)}
+              key={i}
+              className={i === selected ? "selected" : null}
+              onClick={() => setSelected(i)}
             >
-              {hook.label}
+              {hook}
             </li>
           ))}
         </ul>
---
+++ src/UseReducer/UseReducer.js
@@ -0,0 +1,46 @@
+import { useState, useReducer } from "react";
+
+import "./UseReducer.css";
+
+const initialState = {
+  count: 0,
+  showText: false,
+};
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "inc":
+      return {
+        ...state,
+        count: state.count + 1,
+      };
+
+    case "toggle":
+      return {
+        ...state,
+        showText: !state.showText,
+      };
+
+    default:
+      return state;
+  }
+};
+
+const UseReduces = () => {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  const onClick = () => {
+    dispatch({ type: "inc" });
+    dispatch({ type: "toggle" });
+  };
+
+  return (
+    <div className="container">
+      <h2>{state.count}</h2>
+      <button onClick={onClick}>Increment</button>
+      {state.showText && <h2>This is a text</h2>}
+    </div>
+  );
+};
+
+export default UseReduces;
